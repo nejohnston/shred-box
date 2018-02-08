fceda69eec973973330450d99b3dc526192b7b09
michaelujkim <michaelujkim@gmail.com>
2018-02-08 14:50:45 -0800

    test

--- imports/ui/components/NextUpDisplay/index.js
+++ imports/ui/components/NextUpDisplay/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 function colorizeAnswer(answer) {
-  if (answer === 0) {
+  if (answer == 0) {
     return <div className="red-answer">red</div>;
   }
-  if (answer === 1) {
+  if (answer == 1) {
     return <div className="blue-answer">blue</div>;
   }
-  if (answer === 2) {
+  if (answer == 2) {
     return <div className="green-answer">green</div>;
   } else {
     return <div className="purple-answer">purple</div>;
