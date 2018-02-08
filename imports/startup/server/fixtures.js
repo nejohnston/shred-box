238a6b5c6b6cac5c4368435245ca9f289b881c91
qingxin-yuan <qingxin.yn@gmail.com>
2018-02-08 13:21:31 -0800

    Insert userid to collection.

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,18 +1,14 @@
-import { Meteor } from 'meteor/meteor';
-import {Accounts} from 'meteor/accounts-base';//for add default user
-import {Players} from '../../api/players';
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base"; //for add default user
+import { Players } from "../../api/players";
 
 Meteor.startup(() => {
-
-  if (Meteor.users.find().count() ===0 ) {
-
+  if (Meteor.users.find().count() === 0) {
     let defaultUser = Accounts.createUser({
       email: "test@test.com",
       password: "password"
-    })
+    });
 
-    Players.insert({ userId: defaultUser});
-
-
+    Players.insert({ userId: defaultUser });
   }
 });
