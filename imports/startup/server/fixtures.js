114470938b39ea0b75c9a56f126e01151f4f9552
qingxin-yuan <qingxin.yn@gmail.com>
2018-02-08 10:46:20 -0800

    Get rid of extra files in the boilerplate.

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,34 +1,13 @@
-// Fill the DB with example data on startup
-
 import { Meteor } from 'meteor/meteor';
-import { Links } from '../../api/links/links.js';
+import {Accounts} from 'meteor/accounts-base';//for add default user
 
 Meteor.startup(() => {
-  // if the Links collection is empty
-  if (Links.find().count() === 0) {
-    const data = [
-      {
-        title: 'Do the Tutorial',
-        url: 'https://www.meteor.com/try',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Follow the Guide',
-        url: 'http://guide.meteor.com',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Read the Docs',
-        url: 'https://docs.meteor.com',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Discussions',
-        url: 'https://forums.meteor.com',
-        createdAt: new Date(),
-      },
-    ];
 
-    data.forEach(link => Links.insert(link));
+  if (Meteor.users.find().count() ===0 ) {
+
+    let defaultUser = Accounts.createUser({
+      email: "test@test.com",
+      password: "password"
+    })
   }
 });
