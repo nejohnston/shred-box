7aca27c2eb5ba6d758900e532974b178b4bc29c9
Sstobo <seanstobo@gmail.com>
2018-02-08 13:32:10 -0800

    test

--- imports/ui/containers/App.js
+++ imports/ui/containers/App.js
@@ -1,34 +1,15 @@
-<< << << < HEAD
-import React, {
-	Component
-} from "react";
-import PropTypes from "prop-types";
-import {
-	withTracker
-} from "meteor/react-meteor-data";
-import "./styles.css"; ===
-=== =
-import React, {
-	Component
-} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-	withTracker
-} from 'meteor/react-meteor-data';
-import './styles.css'; >>>
->>> > e5f12b403a7b8f8f9ad57faed8576f0ceb63dffe
+import { withTracker } from 'meteor/react-meteor-data';
+import './styles.css';
 
-import {
-	Players
-} from '../../api/players';
+import { Players } from '../../api/players';
 
-import {
-	Meteor
-} from "meteor/meteor";
-import BlueButton from "../components/BlueButton";
-import GreenButton from "../components/GreenButton";
-import PurpleButton from "../components/PurpleButton";
-import RedButton from "../components/RedButton";
+import { Meteor } from 'meteor/meteor';
+import BlueButton from '../components/BlueButton';
+import GreenButton from '../components/GreenButton';
+import PurpleButton from '../components/PurpleButton';
+import RedButton from '../components/RedButton';
 
 import AccountsWrapper from '../components/AccountsWrapper';
 
@@ -38,42 +19,29 @@ class App extends Component {
 	}
 
 	render() {
-		return ( <
-			div className = "input-wrapper" >
-			<
-			div className = "top-wrapper" >
-			<
-			div className = "top-left" > COMING UP: (icon)(icon)(icon)(icon) < /div> <
-			div className = "top-right" > SCORE: 1 / 12 < /div> <
-			/div> <
-			div className = "bottom-wrapper" >
-			<
-			div className = "div1" >
-			<
-			RedButton / >
-			<
-			/div>
+		return (
+			<div className="input-wrapper">
+				<div className="top-wrapper">
+					<div className="top-left">COMING UP: (icon) (icon) (icon) (icon)</div>
+					<div className="top-right">SCORE : 1/12 </div>
+				</div>
+				<div className="bottom-wrapper">
+					<div className="div1">
+						<RedButton />
+					</div>
 
-			<
-			div className = "div2" >
-			<
-			BlueButton / >
-			<
-			/div>
+					<div className="div2">
+						<BlueButton />
+					</div>
 
-			<
-			div className = "div3" >
-			<
-			div className = "green-button" / >
-			<
-			/div> <
-			div className = "div4" >
-			<
-			div className = "purple-button" / >
-			<
-			/div> <
-			/div> <
-			/div>
+					<div className="div3">
+						<div className="green-button" />
+					</div>
+					<div className="div4">
+						<div className="purple-button" />
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
