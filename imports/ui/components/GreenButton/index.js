13891a827746e8a6488d066840b97b85a324e43c
michaelujkim <michaelujkim@gmail.com>
2018-02-08 11:53:35 -0800

    added button componenents

---
+++ imports/ui/components/GreenButton/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GreenButton = ({ greenInput }) => (
+  <div onClick={greenInput} className="green-button">
+    Green
+  </div>
+);
+
+GreenButton.propTypes = {
+  greenInput: PropTypes.func.isRequired
+};
+
+export default GreenButton;
---
+++ imports/ui/components/RedButton/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const RedButton = ({ redInput }) => (
+  <div onClick={redInput} className="red-button">
+    Red
+  </div>
+);
+
+RedButton.propTypes = {
+  redInput: PropTypes.func.isRequired
+};
+
+export default RedButton;
