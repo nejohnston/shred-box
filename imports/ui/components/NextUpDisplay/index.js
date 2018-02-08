c7cf17aeaf0473d10d09d09fe7d838c1d65294f4
michaelujkim <michaelujkim@gmail.com>
2018-02-08 14:30:23 -0800

    roughed out nextupdisplay

---
+++ imports/ui/components/NextUpDisplay/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NextUpDisplay = ({ answer }) => <div>{answer}</div>;
+export default NextUpDisplay;
--- imports/ui/components/PurpleButton/index.js
+++ imports/ui/components/PurpleButton/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const purpleClicked = (answer) => {
-  console.log(answer)
+const purpleClicked = answer => {
+  console.log(answer);
   if (answer === 3) {
-  console.log("correct")
+    console.log("correct");
   } else {
-    console.log("wrong")
+    console.log("wrong");
   }
 };
 
-const PurpleButton = ({answer}) => (
-  <div onClick={() => {purpleClicked(answer)}}
-  
-  className="purple-button">
-
-    Blue
-  </div>
+const PurpleButton = ({ answer }) => (
+  <div
+    onClick={() => {
+      purpleClicked(answer);
+    }}
+    className="purple-button"
+  />
 );
 export default PurpleButton;
