9cc32365bf642ecc4d7267370d20470586f37e1c
Sstobo <seanstobo@gmail.com>
2018-02-08 15:50:01 -0800

    working on scoreboard

--- imports/ui/components/NextUpDisplay/index.js
+++ imports/ui/components/NextUpDisplay/index.js
@@ -10,12 +10,7 @@ function colorizeAnswer({ answer }) {
   if (answer == 2) {
     return <div className="green-answer"></div>;
   } else {
-<<<<<<< HEAD
     return <div className="purple-answer"></div>;
-=======
-    console.log(answer);
-    return <div className="purple-answer">purple</div>;
->>>>>>> 9749d829c255bd1c33bd2e9c5a752093487cefe7
   }
 }
 const NextUpDisplay = ({ answer }) => colorizeAnswer({ answer });
