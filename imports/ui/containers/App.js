aff769e832dd073af8ae01e3e398e2c32cfa1d0b
Sstobo <seanstobo@gmail.com>
2018-02-08 14:24:41 -0800

    answer variable

--- imports/ui/containers/App.js
+++ imports/ui/containers/App.js
@@ -1,31 +1,33 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withTracker } from "meteor/react-meteor-data";
-import "./styles.css";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import './styles.css';
 
-import { Players } from "../../api/players";
+import { Players } from '../../api/players';
 
-import { Meteor } from "meteor/meteor";
-import BlueButton from "../components/BlueButton";
-import GreenButton from "../components/GreenButton";
-import PurpleButton from "../components/PurpleButton";
-import RedButton from "../components/RedButton";
+import { Meteor } from 'meteor/meteor';
+import BlueButton from '../components/BlueButton';
+import GreenButton from '../components/GreenButton';
+import PurpleButton from '../components/PurpleButton';
+import RedButton from '../components/RedButton';
 
-import AccountsWrapper from "../components/AccountsWrapper";
+import AccountsWrapper from '../components/AccountsWrapper';
+
+
 
 class App extends Component {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  render() {
+	render() {
     const randomArray = (length, max) => {
-      return Array.apply(null, Array(length)).map(function() {
+      return Array.apply(null, Array(length)).map(function () {
         return Math.round(Math.random() * max);
       });
-    };
-
-    let array = randomArray(4, 3);
+    }
+    
+    let array = randomArray(4,3);
     let answer = array[0];
     
     console.log(answer)
@@ -41,20 +43,9 @@ class App extends Component {
 						<RedButton answer={answer}/>
 					</div>
 
-    return (
-      <div className="input-wrapper">
-        <div className="top-wrapper">
-          <div className="top-left">COMING UP: (icon) (icon) (icon) (icon)</div>
-          <div className="top-right">SCORE : 1/12 </div>
-        </div>
-        <div className="bottom-wrapper">
-          <div className="div1">
-            <RedButton />
-          </div>
-
-          <div className="div2">
-            <BlueButton answer={answer} />
-          </div>
+					<div className="div2">
+						<BlueButton answer={answer}/>
+					</div>
 
 					<div className="div3">
 						<GreenButton answer={answer}/>
@@ -69,9 +60,9 @@ class App extends Component {
 }
 
 export default withTracker(() => {
-  return {
-    currentUser: Meteor.user(),
-    currentUserId: Meteor.userId(),
-    players: Players.find({}).fetch()
-  };
+	return {
+		currentUser: Meteor.user(),
+		currentUserId: Meteor.userId(),
+		players: Players.find({}).fetch()
+	};
 })(App);
