2cb7ece5aa96f2c47b3441e370132bafd62a578e
Sstobo <seanstobo@gmail.com>
2018-02-08 13:26:28 -0800

    blue button

--- imports/ui/containers/App.js
+++ imports/ui/containers/App.js
@@ -14,24 +14,9 @@ import RedButton from "../components/RedButton";
 class App extends Component {
   constructor() {
     super();
+    
   }
-
-<<<<<<< HEAD
-// handleClick(buttonColor) {
-//   console.log(buttonColor)
-// }
-// 	render() {
-		return (
-			<div className="input-wrapper">
-				<div className="top-wrapper">
-					<div className="top-left">COMING UP: (icon) (icon) (icon) (icon)</div>
-					<div className="top-right">SCORE : 1/12 </div>
-				</div>
-				<div className="bottom-wrapper">
-					<div className="div1">
-						<div className="red-button" />
-					</div>
-=======
+  
   render() {
     return (
       <div className="input-wrapper">
@@ -43,10 +28,9 @@ class App extends Component {
           <div className="div1">
             <RedButton />
           </div>
->>>>>>> 13891a827746e8a6488d066840b97b85a324e43c
 
           <div className="div2">
-            <BlueButton />
+            <BlueButton/>
           </div>
 
           <div className="div3">
