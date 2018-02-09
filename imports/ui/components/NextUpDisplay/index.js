cf509af94de7455216b13fb3b5069a75218c46e8
michaelujkim <michaelujkim@gmail.com>
2018-02-09 11:19:09 -0800

    test

--- imports/ui/components/NextUpDisplay/index.js
+++ imports/ui/components/NextUpDisplay/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 function colorizeAnswer({ answer }) {
-  if (answer == 0) {
-    return <div className="red-answer" />;
-  }
-  if (answer == 1) {
-    return <div className="blue-answer" />;
-  }
-  if (answer == 2) {
-    return <div className="green-answer" />;
-  } else {
-    return <div className="purple-answer" />;
-    console.log(answer);
-  }
+  for (const value of answer)
+    (function(value) {
+      if (value == 0) {
+        return <div className="red-answer" />;
+      }
+      if (value == 1) {
+        return <div className="blue-answer" />;
+      }
+      if (value == 2) {
+        return <div className="green-answer" />;
+      } else {
+        console.log(value);
+        return <div className="purple-answer" />;
+      }
+    });
 }
 const NextUpDisplay = ({ answer }) => colorizeAnswer({ answer });
 export default NextUpDisplay;
