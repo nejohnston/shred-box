b848c3b4b254b01f2f5224cb5731d71d55d10ad2
Sstobo <seanstobo@gmail.com>
2018-02-09 13:24:40 -0800

    fix

--- imports/ui/components/GreenButton/index.js
+++ imports/ui/components/GreenButton/index.js
@@ -2,14 +2,20 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import Correct from '../Correct'
 import { Meteor } from 'meteor/meteor';
+// var Sound = require('react-sound').default;
+import Sound from 'react-sound';
 
 const greenClicked = (answer) => {
   console.log(answer)
   if (answer === 2) {
-  alert("correct");
-
+    
   } 
   else {
+      
+      const snd = new Audio("BlueHat.wav"); // buffers automatically when created
+     snd.play();
+     
+   
     console.log("wrong")
   }
 };
