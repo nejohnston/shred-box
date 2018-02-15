b308642b75c6b96fe84dedefa177c036f958e02a
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-15 14:25:35 -0800

    pulled updateScore function

--- imports/api/score.js
+++ imports/api/score.js
@@ -2,17 +2,29 @@ import { Mongo } from "meteor/mongo";
 
 export const Score = new Mongo.Collection("score");
 
+const updateScore = 0;
+const lives = 3;
+
 if (Meteor.isServer) {
   Meteor.publish("score", function scorePublication() {
     return Score.find({});
   });
 }
 
+<<<<<<< HEAD
 // const initScore = 3;
 
 Meteor.methods({
   "score.updateScore"(score){
     Score.update({id: 1}, {$set:{score: score+1}})
+=======
+Meteor.methods({
+  "score.updateScore"() {
+    if (score === 10) {
+      alert;
+    }
+    // Streamy.broadcast(core);
+>>>>>>> updated updateScore method
   },
 
   "score.updateLives"() {}
