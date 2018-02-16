7c0e117d4e97f91684d7f4084938977f2fdcc158
michaelujkim <michaelujkim@gmail.com>
2018-02-15 16:18:30 -0800

    alert and reset when lives go to zero

--- imports/ui/components/BlueButton/index.js
+++ imports/ui/components/BlueButton/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const blueClicked = (noteChoice, score, lives) => {
-  // if (!this.props.lives===0){
-  if (noteChoice === 1) {
-    const snd3 = new Audio("GreenPerc2.wav");
-    snd3.play();
-    Meteor.call("score.updateScore", score);
+  if (!lives == 0) {
+    if (noteChoice === 1) {
+      const snd3 = new Audio("GreenPerc2.wav");
+      snd3.play();
+      Meteor.call("score.updateScore", score);
+    } else {
+      const errorsnd = new Audio("record-scratch.mp3");
+      console.log("wrong");
+      errorsnd.play();
+      Meteor.call("score.updateLives", lives);
+    }
   } else {
-    const errorsnd = new Audio("record-scratch.mp3");
-    console.log("wrong");
-    errorsnd.play();
-    Meteor.call("score.updateLives", lives);
+    alert("You lost, your score is :", score);
+    Meteor.call("songs.reset");
   }
 };
 
