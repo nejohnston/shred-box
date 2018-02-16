cc413ac523179fb7c0b5daf64063ef0034bccea0
qingxin-yuan <qingxin.yn@gmail.com>
2018-02-16 15:18:19 -0800

    Add login router.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -19,3 +19,25 @@
 //     BlazeLayout.render('App_body', { main: 'App_notFound' });
 //   },
 // };
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import createBrowserHistory from "history/createBrowserHistory";
+
+import App from "../../ui/containers/App";
+import Login from "../../ui/components/AccountsWrapper";
+import NotFound from "../../ui/components/NotFound/";
+
+// const browserHistory = createBrowserHistory();
+
+export const renderRoutes = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
