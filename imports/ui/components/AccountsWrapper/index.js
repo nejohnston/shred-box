1d12a598c7435ff9d49e756a93f6bacb0a4b29f9
michaelujkim <michaelujkim@gmail.com>
2018-02-18 18:32:52 -0800

    testing out adding instrucitions

--- imports/ui/components/AccountsWrapper/index.js
+++ imports/ui/components/AccountsWrapper/index.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Template } from 'meteor/templating';
-import { Blaze } from 'meteor/blaze';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Template } from "meteor/templating";
+import { Blaze } from "meteor/blaze";
 
-import './style.css';
+import "./style.css";
 
 export default class AccountsWrapper extends Component {
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
-    this.view = Blaze.render(Template.loginButtons,
-      ReactDOM.findDOMNode(this.refs.container));
+    this.view = Blaze.render(
+      Template.loginButtons,
+      ReactDOM.findDOMNode(this.refs.container)
+    );
+    // this.instructions = Blaze.render(
+    //   Template.instructions,
+    //   ReactDOM.findDOMNode(this.refs.container)
+    // );
   }
   componentWillUnmount() {
-    Blaze.remove(this.view); // Clean up Blaze view
+    Blaze.remove(this.view);
+    // Blaze.remove(this.instructions); // Clean up Blaze view
   }
   render() {
     return <span ref="container" />; // Render a placeholder
