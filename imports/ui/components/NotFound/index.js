037bc103b6c2920922f7b67848b514a15d7421a4
qingxin-yuan <qingxin.yn@gmail.com>
2018-02-18 21:42:32 -0800

    Install missing eslint packages

--- imports/ui/components/NotFound/index.js
+++ imports/ui/components/NotFound/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-export default class NotFound extends Component{
-  render() {
-    return <p> Not Found</p>
-  }
+export default class NotFound extends Component {
+    render() {
+        return <p> Not Found</p>;
+    }
 }
