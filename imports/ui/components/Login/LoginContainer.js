e7c53dfe8190f11bdc3706cc79d3a9ae7b3be404
michaelujkim <michaelujkim@gmail.com>
2018-02-18 18:46:31 -0800

    added instructions to login page

--- imports/ui/components/Login/LoginContainer.js
+++ imports/ui/components/Login/LoginContainer.js
@@ -4,18 +4,26 @@ import { withRouter } from "react-router";
 import { withTracker } from "meteor/react-meteor-data";
 
 import Login from "./Login";
+import Instructions from "./Instructions";
 
 class LoginContainer extends Component {
   render() {
     // if (Meteor.isClient) {
-      const from = "/";
-      console.log(this.props.currentUserId);
-      return !this.props.currentUserId ? <Login /> : <Redirect to={from} />;
+    const from = "/";
+    console.log(this.props.currentUserId);
+    return !this.props.currentUserId ? (
+      <div>
+        <Login />
+        <Instructions />
+      </div>
+    ) : (
+      <Redirect to={from} />
+    );
     // }
   }
 }
 export default withTracker(() => {
-  return { 
-    currentUserId: Meteor.userId() 
+  return {
+    currentUserId: Meteor.userId()
   };
 })(LoginContainer);
